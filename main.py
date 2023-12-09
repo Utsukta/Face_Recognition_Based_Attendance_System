0e3c9e136c0975c639b60a77d953bd4fdead58d1
Utsukta Khatri <utsukta99kc@gmail.com>
2023-12-09 21:34:51 +0545

    shared aded

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import tkinter;
+from tkinter import ttk
+from PIL import Image
 
-m=tkinter.Tk()
-m.title('Face Recognition system')
-tkinter.Button().pack()
+class Face_Recogniton_Attendance_System:
+    def __init__(self, root):
+        self.root=root
+        self.root.geometry("1440x900+0+0")
+        self.root.title("Face Recognition system")
+        self.root.configure(bg="white")
 
-m.mainloop()
+
+
+if __name__== "__main__":
+    root=tkinter.Tk()
+    obj=Face_Recogniton_Attendance_System(root)
+    root.mainloop()
+
+
---
+++ shared.py
@@ -0,0 +1,7 @@
+import tkinter;
+from tkinter import ttk
+from PIL import Image
+
+class Shared:
+    def __init__(self, root):
+        self.root= root
