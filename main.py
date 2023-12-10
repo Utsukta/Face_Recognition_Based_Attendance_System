60c4fca7880da498bb1ad0733b9ef2d7533d5e26
Utsukta Khatri <utsukta99kc@gmail.com>
2023-12-10 10:51:56 +0545

    add images

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-import tkinter;
-from tkinter import ttk
-from PIL import Image
+from tkinter import *
+from PIL import Image, ImageTk
+from shared import Shared
 
-class Face_Recogniton_Attendance_System:
+class Face_Recognition_Attendance_System:
     def __init__(self, root):
-        self.root=root
+        self.root = root
         self.root.geometry("1440x900+0+0")
         self.root.title("Face Recognition system")
         self.root.configure(bg="white")
 
+        # Create an instance of the Shared class
+        self.shared_instance = Shared(self.root)
+        
 
-
-if __name__== "__main__":
-    root=tkinter.Tk()
-    obj=Face_Recogniton_Attendance_System(root)
+if __name__ == "__main__":
+    root = Tk()
+    obj = Face_Recognition_Attendance_System(root)
     root.mainloop()
-
-
--- shared.py
+++ shared.py
@@ -1,7 +1,16 @@
-import tkinter;
-from tkinter import ttk
-from PIL import Image
+from tkinter import *
+from PIL import Image, ImageTk
 
 class Shared:
     def __init__(self, root):
-        self.root= root
+        self.root = root
+        self.content_frame = Frame(root, bg="pink")
+        self.content_frame.place(x=0, y=0, width=120, height=900)
+        img = Image.open("/Users/utsuktakhatri/Desktop/Face_recogniton_system/Images/rps_logo-preview.png")
+        img=img.resize((100,50))
+        self.photoimg = ImageTk.PhotoImage(img)
+        f_lbl = Label(self.root, image=self.photoimg,bg='pink')
+        f_lbl.place(x=5, y=20)
+    
+
+    
