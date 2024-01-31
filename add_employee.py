52305bb052384694451d9f3a4e7b70ea112528c0
Utsukta Khatri <utsukta99kc@gmail.com>
2024-01-31 20:16:28 +0545

    Moddified

--- add_employee.py
+++ add_employee.py
@@ -279,8 +279,6 @@ class AddEmployee:
          conn.commit()
          conn.close()
          
-
-         
      #============get cursor===========#    
     def get_cursor(self , event=""):
         cursor_focus=self.employee_table.focus()
