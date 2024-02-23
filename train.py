9af28fb77775ef97170cdcfb9da18028cd188d72
Utsukta Khatri <utsukta99kc@gmail.com>
2024-02-23 22:28:35 +0545

    Data Training Completed

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ from PIL import Image, ImageTk
 from constants import Constants
 import mysql.connector
 import cv2
+import numpy as np
 
 class Train:
     def __init__(self, root):
@@ -44,6 +45,27 @@ class Train:
         for image in path:
             #Converts in grey scale image
             img=Image.open(image).convert("L")
+
+            #To convert in grid scale used numpy
+            imageNp=np.array(img,'uint8')
+            id=int(os.path.split(image)[1].split('.')[1])
+            # id = int(os.path.splitext(os.path.split(image)[1])[1])
+
+            print(id)
+
+            faces.append(imageNp)
+            ids.append(id)
+            cv2.imshow("Training",imageNp)
+            cv2.waitKey(1)==13
+
+        ids=np.array(ids)
+
+        #-------------Train Classifier-----------------
+        clf = cv2.face.LBPHFaceRecognizer_create()
+        clf.train(faces,ids)
+        clf.write("classifier.xml")
+        cv2.destroyAllWindows()
+        messagebox.showinfo("Result","Training datasets completed")
 
 
 if __name__ == "__main__":
