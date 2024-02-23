db8ccee9e0830eb7513d67b3b6951912c1b617f3
Utsukta Khatri <utsukta99kc@gmail.com>
2024-02-23 23:11:42 +0545

    Modified

--- face_recognition.py
+++ face_recognition.py
@@ -31,9 +31,60 @@ class Facerecognition:
         save_btn=Button(btn_frame, text="Face Detector",font=(Constants.Add_Employee_font ,15),highlightthickness=0)
         save_btn.grid(row=0,column=1)
 
+    def face_recognizer(self):
+        def draw_boundary(img,classifier,scaleFactor,minNeighbors,color,text,clf):
+            gray_image=cv2.cvtColor(img,cv2.COLOR_BAYER_BGGR2GRAY)
+            features=classifier.detectMultiScale(gray_image,scaleFactor,minNeighbors)
 
-    
+            coord=[]
 
+            for  x,y,w,h in features:
+                cv2.rectangle(img(x,y),(x+w,y+h),(0,255,0),3)
+                id,predict=clf.predict(gray_image[y:y + h, x:x + w])
+                # confidence=int((100*(1-predict/300)))
+                confidence=(float("%.2f" %(id)))
+
+                conn=mysql.connector.connect(host="localhost",username="root",password="Cre@ture12;",database="face_recognizer")
+                my_cursor=conn.cursor()
+                my_cursor.execute("SELECT Name from employee WHERE employee_id="+str((id)))
+                n=my_cursor.fetchone()
+                n="+".join(n)
+
+                my_cursor.execute("SELECT department from employee WHERE employee_id="+str((id)))
+                d=my_cursor.fetchone()
+                d="+".join(d)
+
+
+
+                if confidence>77:
+                    cv2.putText(img,f"Name:{n}",(x,y-55),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
+                    cv2.putText(img,f"department:{d}",(x,y-30),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
+                else:
+                    cv2.rectangle(img(x,y),(x+w,y+h),(0,0,255),3)
+                    cv2.putText(img,"Unknown Face",(x,y-5),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
+
+                coord=[x,y,w,h]
+            return coord
+        
+
+        def recognize(img,clf,faceCascade):
+            coord=draw_boundary(img,faceCascade,1,1,10,(255,255,255),"Face",clf)
+            return img
+        
+        faceCascade=cv2.CascadeClassifier("haarcascade_profileface_default.xml")
+        clf=cv2.face.LBPHFaceRecognizer_create()
+        clf.read("classifier.xml")
+
+        video_capture=cv2.VideoCapture(0)
+
+        while True:
+            ret,img=video_capture.read()
+            img=recognize(img,clf,faceCascade)
+            cv2.imshow("Welcome to face Reconition",img)
+            if cv2.waitKey(13):
+                break
+            video_capture.release()
+            cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     root = tk.Tk()
