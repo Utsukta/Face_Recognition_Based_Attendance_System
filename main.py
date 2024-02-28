45d693f4e695657705535be788c263eb23342118
Utsukta Khatri <utsukta99kc@gmail.com>
2024-02-28 22:39:04 +0545

    Modified

--- main.py
+++ main.py
@@ -22,8 +22,7 @@ class Face_Recognition_Attendance_System:
 
         home_img_label=Label(image=self.home_img)
         home_img_label.place(x=190,y=0)
-       
-        
+         
 
 if __name__ == "__main__":
     root = Tk()
